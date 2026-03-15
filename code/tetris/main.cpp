f5b4f96b145e2c287e28753628b8be8a4061c957
Coffee0xff_256 <mateo.rodriguez5@udea.edu.co>
2026-03-14 22:39:05 -0500

    organizo la logica principal de mi main que estaba un poco loca

--- code/tetris/main.cpp
+++ code/tetris/main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int main()
 {
-    int alto = 8, ancho = 32;
-    unsigned char **prueba = tablero(alto,ancho);
+    int alto = 0, ancho = 0;
+    cout << "Welcome to my Tetris" << endl;
+    cout << "Please put a number (ancho debe ser multiplo de 8):" << endl;
+    cout << "Alto: ";
+    cin >> alto;
+    cout << "Ancho: ";
+    cin >> ancho;
 
+    //aqui creamos nuestro tablero
+    unsigned char **tablero_tetris = tablero(alto,ancho);
 
-    delete prueba;
+    //aqui ira la logica de nuestro juego
+
+    // limpio memoria dinamica
+    delete[] tablero_tetris;
     cout << "Hello World!" << endl;
 
     return 0;
